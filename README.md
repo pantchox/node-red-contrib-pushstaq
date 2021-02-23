567a7f603976931c4954ba066c719be0849a7474
PushStaq <aryeidc@gmail>
2021-02-23 10:46:57 +0200

    first commit of working PushStaq node

--- README.md
+++ README.md
@@ -4,12 +4,14 @@ This is the official PushStaq node for Node-Red.
 
 With this node you can trigger and alert real time push notifications messages from your Node-Red flows to any device with PushStaq.
 
+To use PushStaq you can quickly register at https://www.pushstaq.com and start pushing message in less then 1 minute setup!
+
 ## Guidelines
 
 In PushStaq
 
 -   simply create a channel or select existing one you would like to push messages to from Node-Red.
--   In the channel open the settings pane and copy the API key. If API key does not exist click the "generate API key" button, this will create a new API key dedicated to the selected channel
+-   In the channel open the settings pane and copy the API key. If API key does not exist click the "Generate API Key" button, this will create a new API key dedicated to the selected channel
 
 In Node-Red
 
---
+++ pushstaq.js
@@ -0,0 +1,111 @@
+module.exports = function (RED) {
+    const https = require('https');
+    // const PUSHSTAQ_API_URL = 'https://www.pushstaq.com/api/push/';
+
+    // function to send the payload to PushStaq
+    function callPushStaqApi(node, msg, done) {
+        // create valid PushStaq API message
+        const data = JSON.stringify({
+            message: msg.payload,
+        });
+
+        // use node native https lib
+        const req = https
+            .request(
+                {
+                    host: 'www.pushstaq.com',
+                    port: '443',
+                    path: '/api/push/',
+                    method: 'POST',
+                    headers: {
+                        'x-api-key': node.config.apikey,
+                        'Content-Type': 'application/json',
+                        'Content-Length': data.length,
+                    },
+                },
+                function (res) {
+                    let resData = '';
+
+                    res.on('data', function (chunk) {
+                        resData += chunk;
+                    });
+
+                    res.on('end', function () {
+                        let body = {};
+                        try {
+                            body = JSON.parse(resData);
+                        } catch (e) {
+                            // could not parse json, valid response is JSON means we have an error
+                            body = {error: resData};
+                        }
+                        if (res.statusCode === 201) {
+                            node.warn('PushStaq request success: ' + (body.status ? body.status : body));
+                            if (done) {
+                                done();
+                            }
+                        } else {
+                            const errorMessage = 'PushStaq request error: ' + body.error;
+                            if (done) {
+                                done(errorMessage);
+                            } else {
+                                node.error(errorMessage, msg);
+                            }
+                        }
+                    });
+                }
+            )
+            .on('error', function (err) {
+                const errorMessage = 'PushStaq request unknown error: ' + err.toString();
+                if (done) {
+                    done(errorMessage);
+                } else {
+                    node.error(errorMessage, msg);
+                }
+            });
+
+        req.write(data);
+        req.end();
+    }
+
+    // main node code
+    function PushStaqNode(n) {
+        RED.nodes.createNode(this, n);
+        const node = this;
+        this.config = RED.nodes.getCredentials(n.config);
+        if (!node.config || (node.config && !node.config.apikey)) {
+            node.error('API Key is not defined, aborting.');
+            return;
+        }
+
+        this.on('input', function (msg, send, done) {
+            msg.payload = msg.payload || null;
+
+            if (msg.payload) {
+                callPushStaqApi(node, msg, done);
+            } else {
+                if (done) {
+                    done('PushStaq node must receive msg.payload in order to work');
+                } else {
+                    node.error('PushStaq node must receive msg.payload in order to work', msg);
+                }
+            }
+        });
+    }
+
+    // register node
+    RED.nodes.registerType('PushStaq', PushStaqNode);
+
+    // configuration node
+    function PushStaqApiKeys(n) {
+        RED.nodes.createNode(this, n);
+        this.name = n.name;
+        this.apikey = n.apikey;
+    }
+
+    // register configuration node
+    RED.nodes.registerType('pushstaq-api-keys', PushStaqApiKeys, {
+        credentials: {
+            apikey: {type: 'text'},
+        },
+    });
+};
